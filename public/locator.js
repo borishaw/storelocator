2b8c0ce7300fb7d1c8612ee44a34d50bf0990a18
jbarkun <jbarkun@gmail.com>
2016-05-11 16:19:33 -0400

    Map Styling

--- public/locator.js
+++ public/locator.js
@@ -18,12 +18,22 @@ google.maps.event.addDomListener(window, 'load', function () {
         }
     });
 
+	 var styles = [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"geometry.fill","stylers":[{"visibility":"on"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#b4d4e1"},{"visibility":"on"}]}];
+	 
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         center: new google.maps.LatLng(43.7181557,-79.5181424),
         zoom: 10,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     });
-
+	
+		map.setOptions({styles: styles});
+		
+		//var mapMarker = new google.maps.Marker({
+		//	position: latLng,
+		//	map: map,
+		//	icon: 'img/map-marker.png'
+		// });
+	 
     var panelDiv = document.getElementById('panel');
 
     var dataFeed = new storeLocator.StaticDataFeed;
