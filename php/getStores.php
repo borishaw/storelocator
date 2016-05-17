b36220226f9de44634655a62039af33b91f75fe2
Boris Xiao <borishaw@gmail.com>
2016-05-17 16:08:47 -0400

    push

--- php/getStores.php
+++ php/getStores.php
@@ -7,6 +7,6 @@ DB::$user = 'root';
 DB::$password = 'root';
 DB::$dbName = 'hd_stores';
 
-$stores = DB::query("SELECT * FROM store_info");
+$stores = DB::query("SELECT * FROM store_info WHERE year_round = 1");
 
 echo json_encode($stores);
--- php/insertStores.php
+++ php/insertStores.php
@@ -16,6 +16,9 @@ $csv = Reader::createFromPath('../csv/stores.csv');
 $csv->setOffset(1);
 $result = $csv->fetchAll();
 
+foreach ($result as $store){
+    var_dump($store);
+};
 
 foreach ($result as $key => $row){
     $banner = $row[0];
@@ -26,27 +29,30 @@ foreach ($result as $key => $row){
     $province = $row[5];
     $postal_code = $row[6];
     $tel = $row[7];
+    $year_round = $row[8];
+    $seasonal = $row[9];
     $query = $address . ' ' . $city. ' ' . $province . ' ' . $postal_code;
     try {
         $query_result = $geocoder->geocode($query)->get(0);
+        $x_coordinate = $query_result->getLatitude();
+        $y_coordinate = $query_result->getLongitude();
+        DB::insert("store_info", [
+            "banner" => $banner,
+            "store_name" => $store_name,
+            "address" => $address,
+            "city" => $city,
+            "province" => $province,
+            "postal_code" => $postal_code,
+            "tel" => $tel,
+            "x_coordinate" => $x_coordinate,
+            "y_coordinate" => $y_coordinate,
+            "year_round" => $year_round,
+            "seasonal" => $seasonal
+        ]);
     } catch (Exception $e){
         $error_message = $e->getMessage();
         print_r($error_message);
     }
-    $x_coordinate = $query_result->getLatitude();
-    $y_coordinate = $query_result->getLongitude();
-    DB::insert("store_info", [
-        "banner" => $banner,
-        "store_id" => $store_id,
-        "store_name" => $store_name,
-        "address" => $address,
-        "city" => $city,
-        "province" => $province,
-        "postal_code" => $postal_code,
-        "tel" => $tel,
-        "x_coordinate" => $x_coordinate,
-        "y_coordinate" => $y_coordinate
-    ]);
     if ($key % 10 == 0) {
         sleep(1);
     }
